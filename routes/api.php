f9c15708c830ebd7eedd7305848897055a54baf0
Macelux <macelux@hotmail.com>
2020-08-02 00:46:57 +0100

    first commit

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('users', 'UsersController@index');
-Route::get('user/{id}', 'UsersController@show');
-Route::post('user', 'UsersController@store');
-Route::post('user_login', 'UsersController@checkLogin');
-Route::put('user', 'UsersController@update');
-Route::delete('user/{id}', 'UsersController@destroy');
+// Route::get('users', 'UsersController@index');
+// Route::get('user/{id}', 'UsersController@show');
+// Route::post('user', 'UsersController@store');
+// Route::post('user_login', 'UsersController@checkLogin');
+// Route::put('user', 'UsersController@update');
+// Route::delete('user/{id}', 'UsersController@destroy');
